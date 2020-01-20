a0cc0293288c4cd93506c992ee176011cc976479
romeoshe <romeoshe@gmail.com>
2020-01-21 01:16:19 +0200

    task 1 - flip image

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
+import classNames from 'classnames'; 
+
 import './Image.scss';
 
 class Image extends React.Component {
@@ -12,6 +14,8 @@ class Image extends React.Component {
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
+    //bind the flipImage method.
+    this.flipImage = this.flipImage.bind(this);
     this.state = {
       size: 200
     };
@@ -35,10 +39,18 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+  //flip the image horizontally (like a mirror).
+  flipImage (){
+    this.setState(({flipped}) => ({flipped: !flipped}));
+  }
   render() {
+    const {flipped} = this.state;
+    //add css to image for flip when flipped is true.
+    const clsRoot = classNames('image-root', {'image-flip': flipped});
+
     return (
       <div
-        className="image-root"
+        className={clsRoot}
         style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
           width: this.state.size + 'px',
@@ -46,7 +58,7 @@ class Image extends React.Component {
         }}
         >
         <div>
-          <FontAwesome className="image-icon" name="arrows-alt-h" title="flip"/>
+        <FontAwesome className="image-icon" onClick={this.flipImage} name="arrows-alt-h" title="flip"/>
           <FontAwesome className="image-icon" name="clone" title="clone"/>
           <FontAwesome className="image-icon" name="expand" title="expand"/>
         </div>
